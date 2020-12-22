2de038da26a79921484d028dd8536c7aba9effc3
Ari <bakalariel2002@gmaill.com>
2020-12-22 19:26:09 -0300

    Gráfico all-time limpio

--- dashboard.php
+++ dashboard.php
@@ -7,62 +7,62 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.js"></script>
   </head>
   <body>
+
+  <div id="general">
+    <div id="1">
     <canvas id="myChart" width="100%"></canvas>
-<script>
-var ctx = document.getElementById('myChart');
-var myChart = new Chart(ctx, {
-    type: 'line',
-    data: {
-        labels: ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'],
-        datasets: [{
-            label: 'Total recaudado',
-            backgroundColor: [
-                'rgba(0, 0, 0, 0)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)'
-            ],
-            pointBackgroundColor: [
-                'rgba(205, 99, 132, 1)',
-                'rgba(205, 99, 132, 1)',
-                'rgba(205, 99, 132, 1)',
-                'rgba(205, 99, 132, 1)',
-                'rgba(205, 99, 132, 1)',
-                'rgba(205, 99, 132, 1)',
-                'rgba(205, 99, 132, 1)',
-                'rgba(205, 99, 132, 1)',
-                'rgba(205, 99, 132, 1)',
-                'rgba(205, 99, 132, 1)',
-                'rgba(205, 99, 132, 1)',
-                'rgba(205, 99, 132, 1)'
-            ],
-            borderWidth: 2,
-            pointRadius: 3.5,
-            data: [30, 50, 10, 5, 10, 40, 35, 25, 15, 10, 35, 55]
-        }]
-    },
-
-    options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true
-                }
+    <script>
+      var ctx = document.getElementById('myChart');
+      var myChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'],
+            datasets: [{
+                label: 'Total recaudado',
+                backgroundColor: [
+                    'rgba(0, 0, 0, 0)'
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)'
+                ],
+                pointBackgroundColor: [
+                  'rgba(205, 99, 132, 1)',
+                  'rgba(205, 99, 132, 1)',
+                  'rgba(205, 99, 132, 1)',
+                  'rgba(205, 99, 132, 1)',
+                  'rgba(205, 99, 132, 1)',
+                  'rgba(205, 99, 132, 1)',
+                  'rgba(205, 99, 132, 1)',
+                  'rgba(205, 99, 132, 1)',
+                  'rgba(205, 99, 132, 1)',
+                  'rgba(205, 99, 132, 1)',
+                  'rgba(205, 99, 132, 1)',
+                  'rgba(205, 99, 132, 1)'
+                ],
+                borderWidth: 2,
+                pointRadius: 3.5,
+                data: [30, 50, 10, 5, 10, 40, 35, 25, 15, 10, 35, 55]
             }]
-        }
-    }
-});
-</script>
+          },
 
-
-
-
-
+          options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            }
+          }
+      });
+    </script>
+    </div>
 <!-- AUTOREFRESH 1MIN
 
     <script type="text/javascript">
       setTimeout(function(){location = ''},60000)
     </script>
 
+-->
   </body>
 </html>
